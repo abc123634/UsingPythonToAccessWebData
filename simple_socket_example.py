3b0c95fb14430c746deb23d89308629272ebebc9
Lee Meng <b98705001@gmail.com>
2015-12-08 20:47:56 +0800

    socket code updated

--- simple_socket_example.py
+++ simple_socket_example.py
@@ -1,10 +1,13 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 8000))
+# s.connect(('127.0.0.1', 8000))
+s.connect(('www.pythonlearn.com', 80))
 
-#no need to incorporate domain name/port number again in the message
-message = 'GET /index.html HTTP/1.0\n\n'
+#no need to incorporate domain name/port number again in the message 
+#(if the server is local host)
+# message = 'GET /index.html HTTP/1.0\n\n'
+message = 'GET http://www.pythonlearn.com/code/intro-short.txt HTTP/1.0\n\n'
 
 #use encoded message as socket parameter for the remote server to understand the request
 s.send(message.encode('utf8'))
